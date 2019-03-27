2f58ee00bebd24663ae5e7d40b9ee4ca4ede2f79
silencender <Demo@silenceender.com>
2019-03-27 10:47:18 +0800

    fix graph

--- networks/faceswap_gan_model.py
+++ networks/faceswap_gan_model.py
@@ -156,18 +156,18 @@ class FaceswapGANModel():
         lay = Input(shape=(output_size, output_size, 3))
         ne = 1
         x = inp
-        x = upscale_block(x, 256//coef, use_norm, norm=norm)
+        x = UpSampling2D()(x)
         ne <<= 1
-        y = Lambda(lambda x: tf.image.resize_images(x, [input_size*ne, input_size*ne]))(lay)
+        y = Lambda(lambda x: K.tf.image.resize_images(x, [input_size*ne, input_size*ne]))(lay)
         x = SPADE_res_block(x, y, 256//coef, True, 'batchnorm')
-        x = upscale_block(x, 128//coef, use_norm, norm=norm)
+        x = UpSampling2D()(x)
         ne <<= 1
-        y = Lambda(lambda x: tf.image.resize_images(x, [input_size*ne, input_size*ne]))(lay)
+        y = Lambda(lambda x: K.tf.image.resize_images(x, [input_size*ne, input_size*ne]))(lay)
         x = SPADE_res_block(x, y, 128//coef, True, 'batchnorm')
         x = self_attn_block(x, 128//coef) if use_self_attn else x
-        x = upscale_block(x, 64//coef, use_norm, norm=norm)
+        x = UpSampling2D()(x)
         ne <<= 1
-        y = Lambda(lambda x: tf.image.resize_images(x, [input_size*ne, input_size*ne]))(lay)
+        y = Lambda(lambda x: K.tf.image.resize_images(x, [input_size*ne, input_size*ne]))(lay)
         x = SPADE_res_block(x, y, 64//coef, True, 'batchnorm')
         #x = res_block(x, 64//coef, norm=norm)
         x = self_attn_block(x, 64//coef) if use_self_attn else conv_block(x, 64//coef, strides=1)
@@ -176,9 +176,9 @@ class FaceswapGANModel():
         activ_map_size = activ_map_size * 8
         while (activ_map_size < output_size):
             outputs.append(Conv2D(3, kernel_size=5, padding='same', activation="tanh")(x))
-            x = upscale_block(x, 64//coef, use_norm, norm=norm)
+            x = UpSampling2D()(x)
             ne <<= 1
-            y = Lambda(lambda x: tf.image.resize_images(x, [input_size*ne, input_size*ne]))(lay)
+            y = Lambda(lambda x: K.tf.image.resize_images(x, [input_size*ne, input_size*ne]))(lay)
             x = SPADE_res_block(x, y, 256//coef, True, 'batchnorm')
             x = conv_block(x, 64//coef, strides=1)
             activ_map_size *= 2
